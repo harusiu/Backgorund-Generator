efe2c8e23c7578eade45f451bdbaafa95297ab4a
Haru Siu <siu910223@gmail.com>
2018-06-20 03:43:15 +0800

    Done!

---
+++ script.js
@@ -0,0 +1,63 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomColorBtn = document.getElementById("randomColorBtn");
+var copyBtn = document.getElementById("copyBtn");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value + ", " + color2.value + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getColorInHex() {
+	var colorInHex = Math.floor((Math.random() * 255) + 1).toString(16);
+	if (colorInHex < 10) {
+		colorInHex = "0" + colorInHex.toString();
+	};
+	return colorInHex;
+}
+
+function randomSingleColor() {
+	var rr = getColorInHex();
+	var gg = getColorInHex();
+	var bb = getColorInHex();
+	var randomColorInHex = "#" + rr + gg + bb;
+	return randomColorInHex;
+}
+
+function randomColors() {
+	color1.value = randomSingleColor();
+	color2.value = randomSingleColor();
+	setGradient();
+}
+
+function copyCssBackground() {
+	var cssBackgroundText = "background: " + css.textContent;
+	//copy text to clipboard without user selecting it
+	//Reference from https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+	var temp = document.createElement('textarea');
+	temp.value = cssBackgroundText;
+	temp.setAttribute("readonly", "");	//||||||||||||||||||||||||||||||||
+	temp.style.position = "absolute";	//without the textarea being shown
+	temp.style.left = "-9999px";		//||||||||||||||||||||||||||||||||
+	document.body.appendChild(temp);
+	temp.select();
+	document.execCommand("copy");
+	document.body.removeChild(temp);
+
+	alert("You've copied this: \n" + cssBackgroundText);
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randomColorBtn.addEventListener("click", randomColors);
+
+copyBtn.addEventListener("click", copyCssBackground);
