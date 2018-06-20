baafbbac39d4539ef95096def57bd2781e6c6200
Haru Siu <siu910223@gmail.com>
2018-06-20 19:33:10 +0800

    update to 3 color selector

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
 var randomColorBtn = document.getElementById("randomColorBtn");
 var copyBtn = document.getElementById("copyBtn");
@@ -8,7 +9,7 @@ var copyBtn = document.getElementById("copyBtn");
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
-	+ color1.value + ", " + color2.value + ")";
+	+ color1.value + ", " + color2.value + ", " + color3.value + ")";
 
 	css.textContent = body.style.background + ";";
 }
@@ -32,15 +33,15 @@ function randomSingleColor() {
 function randomColors() {
 	color1.value = randomSingleColor();
 	color2.value = randomSingleColor();
+	color3.value = randomSingleColor();
 	setGradient();
 }
 
 function copyCssBackground() {
-	var cssBackgroundText = "background: " + css.textContent;
 	//copy text to clipboard without user selecting it
 	//Reference from https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
 	var temp = document.createElement('textarea');
-	temp.value = cssBackgroundText;
+	temp.value = css.textContent;
 	temp.setAttribute("readonly", "");	//||||||||||||||||||||||||||||||||
 	temp.style.position = "absolute";	//without the textarea being shown
 	temp.style.left = "-9999px";		//||||||||||||||||||||||||||||||||
@@ -49,7 +50,7 @@ function copyCssBackground() {
 	document.execCommand("copy");
 	document.body.removeChild(temp);
 
-	alert("You've copied this: \n" + cssBackgroundText);
+	alert("You've copied this: \n" + css.textContent + "\n Just paste it in your css code!");
 }
 
 setGradient();
@@ -57,6 +58,8 @@ setGradient();
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
 
 randomColorBtn.addEventListener("click", randomColors);
 
